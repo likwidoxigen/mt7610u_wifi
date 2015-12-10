3ef22dd73486dd03f62a14f6a2f5eb6c029b9620
glukyne <glukyne@localhost.local>
2015-12-10 23:55:35 +0300

    Added new device ids based on https://wikidevi.com/wiki/MediaTek_MT7610U

--- common/rtusb_dev_id.c
+++ common/rtusb_dev_id.c
@@ -36,9 +36,25 @@
 /* module table */
 USB_DEVICE_ID rtusb_dev_id[] = {
 #ifdef MT76x0
-	{USB_DEVICE(0x148F,0x7610)}, /* MT7610U */
-	{USB_DEVICE(0x0E8D,0x7610)}, /* MT7610U */
-	{USB_DEVICE(0x0b05,0x17d1)}, /* MT7650U USB ID for ASUS AC600*/
+    /* added device ids from https://wikidevi.com/wiki/MediaTek_MT7610U */
+	{USB_DEVICE(0x148F,0x7610)}, /*  MT7610U                            */
+	{USB_DEVICE(0x148F,0x6570)}, /*  MT6570                             */
+	{USB_DEVICE(0x148F,0x6530)}, /*  MT6370                             */
+	{USB_DEVICE(0x0E8D,0x7610)}, /*  MT7610U                            */
+	{USB_DEVICE(0x0E8D,0x7650)}, /*  MT7650U                            */
+	{USB_DEVICE(0x7392,0xA711)}, /*  Edimax EW-7711ULC                  */
+    {USB_DEVICE(0x7392,0xB711)}, /*  (Edimax)                           */
+    {USB_DEVICE(0x148F,0x761A)}, /*  TP-LINK TL-WDN5200/ARCHER T2UH     */
+    {USB_DEVICE(0x148F,0x760A)}, /*  (TP-LINK)                          */
+    {USB_DEVICE(0x0B05,0x17DB)}, /*  (ASUS USB-AC50)                    */
+	{USB_DEVICE(0x0B05,0x17D1)}, /*  (ASUS USB-AC51)                    */
+    {USB_DEVICE(0x0DF6,0x0075)}, /*  Sitecom WLA-3100                   */
+    {USB_DEVICE(0x2019,0xAB31)}, /*  (Planex GW-450D)                   */
+    {USB_DEVICE(0x2001,0x3D02)}, /*  (D-Link DWA-171 rev B1)            */
+    {USB_DEVICE(0x0586,0x3425)}, /*  (ZyXEL) ZyXEL NWD6505              */
+    {USB_DEVICE(0x07B8,0x7610)}, /*  (AboCom) AboCom AU7212             */
+    {USB_DEVICE(0x04BB,0x0951)}, /*  (I-O DATA WN-AC433UK)              */
+    {USB_DEVICE(0x13B1,0x003E)}, /*  (Linksys AE6000)                   */
 	{USB_DEVICE_AND_INTERFACE_INFO(0x0E8D, 0x7630, 0xff, 0x2, 0xff)}, /* MT7630U */
 	{USB_DEVICE_AND_INTERFACE_INFO(0x0E8D, 0x7650, 0xff, 0x2, 0xff)}, /* MT7650U */
 #endif
